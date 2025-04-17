b1fb3f7539be0d59e87fc4d2b5d373280328b3e6
ac17z-admin <abdiel.cruz.p@gmail.com>
2025-04-17 13:25:31 -0600

    Jump added

--- Assets/Scripts/GatherInput.cs
+++ Assets/Scripts/GatherInput.cs
@@ -4,10 +4,17 @@ using UnityEngine.InputSystem;
 
 public class GatterInput : MonoBehaviour
 {
+    //Componentes
     private Controls controls;
-    [SerializeField] private float _valueX; //Serialize me permite visualizar y modificar en el inspector un valor
 
+
+    //Valores
+    [SerializeField] private float _valueX; //Serialize me permite visualizar y modificar en el inspector un valor
     public float ValueX { get => _valueX; } //Enapsulamiento para acceder a la variable
+
+    [SerializeField] private bool _isJumping;
+    public bool IsJumping { get => _isJumping; set => _isJumping = value; }
+    
 
     private void Awake() //Primer método que se manda a llamar 
     {
@@ -19,6 +26,8 @@ public class GatterInput : MonoBehaviour
     {
         controls.Player.Move.performed += StartMove; //Sintaxis para asociar un evento con un mñetodo
         controls.Player.Move.canceled += StopMove; //Revisar documentación para ver los tipos de interaccciones con las entradas
+        controls.Player.Jump.performed += StartJump;
+        controls.Player.Jump.canceled += StopJump;
         controls.Player.Enable();
     }
 
@@ -32,11 +41,22 @@ public class GatterInput : MonoBehaviour
         _valueX = 0;
     }
 
+    private void StartJump(InputAction.CallbackContext context)
+    {
+        _isJumping = true;
+    }
+
+    private void StopJump(InputAction.CallbackContext context)
+    {
+        _isJumping = false; 
+    }
 
     private void OnDisable()
     {
         controls.Player.Move.performed -= StartMove;
         controls.Player.Move.canceled -= StopMove;
+        controls.Player.Jump.performed -= StartJump;
+        controls.Player.Jump.canceled -= StopJump;
         controls.Player.Disable();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,8 +15,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     private int direction = 1;
     private int idSpeed;
+    [SerializeField] private float jumpForce;
 
-    
     void Start() //Obtengo los componentes a los que esta asociado el GameObject
     {
         m_gatherInput = GetComponent<GatterInput>();
@@ -39,6 +39,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Move();
+        Jump();
     }
 
     private void Move()
@@ -54,5 +55,14 @@ public class PlayerController : MonoBehaviour
             m_transform.localScale = new Vector3(-m_transform.localScale.x, m_transform.localScale.y, m_transform.localScale.z);
             direction *= -1;
         }
+    }
+
+    private void Jump()
+    {
+        if(m_gatherInput.IsJumping)
+        {
+            m_rigidbody2D.linearVelocity = new Vector2(speed * m_gatherInput.ValueX, jumpForce);  
+        }
+        m_gatherInput.IsJumping = false;
     }
 }
